2b78b95e64bc668c7cacd392154abc51da6328d7
itsahmadrezza <Ahmadreza.Lashkary.Rad@gmail.com>
2022-06-24 14:21:06 -0400

    remove host selection in create room

--- studybud/settings.py
+++ studybud/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIR = [
+    BASE_DIR / 'static'
+]
+
+# STATIC_ROOT = 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
