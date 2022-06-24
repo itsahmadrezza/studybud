a956d22eca0a795114e4d68b9b2fe2173e3ef068
itsahmadrezza <Ahmadreza.Lashkary.Rad@gmail.com>
2022-06-24 17:44:04 -0400

    install Theme Part 2

--- studybud/settings.py
+++ studybud/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -118,9 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIR = [
-    BASE_DIR / 'static'
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # STATIC_ROOT = 
 
